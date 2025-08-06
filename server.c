0416ecf280f120fdbab3148c6f34bf4468f858bf
Christopher Milan <chrismilan@ucla.edu>
2025-08-05 23:06:04 -0400

    daemon-ify server

--- server.c
+++ server.c
@@ -1,14 +1,14 @@
 #include <sys/signal.h>
 
 #include <arpa/inet.h>
-#include <err.h>
+#include <errno.h>
 #include <limits.h>
 #include <netinet/in.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sysexits.h>
+#include <syslog.h>
 #include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
@@ -48,19 +48,23 @@ static uint8_t s_pk[32]; // my static public key
 static struct advert *map = NULL;
 
 static void handle(braid_t b, int fd) {
+  char ip[INET_ADDRSTRLEN], keystr[65] = {0};
   uint8_t p[PACKET_MAX], es[32], ss[32], nonce[24] = {0};
   struct sockaddr_in sa;
 
   getpeername(fd, (struct sockaddr *)&sa, &(socklen_t){sizeof(sa)});
+  inet_ntop(AF_INET, &sa.sin_addr, ip, sizeof(ip));
 
   if (recv_packet(b, fd, p)) {
-    warn("read from client");
+    syslog(LOG_NOTICE, "[%-15s] request failed: recv: %m", ip);
     goto done;
   }
 
   if (HEAD(p)->type == ATTACH || HEAD(p)->type == ADVERTISE) {
     HandshakeData *data = DATA(p, HandshakeData);
     uint8_t buf[96];
+
+    syslog(LOG_DEBUG, "[%-15s] request received", ip);
 
     // derive (es) shared secret
     crypto_x25519(buf, s_sk, data->e);
@@ -70,7 +74,7 @@ static void handle(braid_t b, int fd) {
     crypto_wipe(buf, 96);
 
     if (crypto_aead_unlock(data->s, HEAD(p)->mac, es, nonce, &HEAD(p)->type, 1, data->s, 32)) {
-      warnx("corrupted packet (s)");
+      syslog(LOG_NOTICE, "[%-15s] corrupted packet (ephemeral)", ip);
       goto done;
     }
     nonce[23]++;
@@ -82,7 +86,7 @@ static void handle(braid_t b, int fd) {
     crypto_wipe(es, 32);
     crypto_wipe(buf, 64);
   } else {
-    warnx("unexpected packet type");
+    syslog(LOG_NOTICE, "[%-15s] unexpected packet type", ip);
     goto done;
   }
 
@@ -91,22 +95,18 @@ static void handle(braid_t b, int fd) {
     AttachData *data = DATA(p, AttachData);
 
     if (crypto_aead_unlock(data->t, data->mac2, ss, nonce, NULL, 0, data->t, 32)) {
-      warnx("corrupted packet (t)");
+      syslog(LOG_NOTICE, "[%-15s] corrupted packet (static)", ip);
       goto done;
     }
     nonce[23]++;
 
     // TODO: check client public key
 
-    printf("attach request from: ");
-    for (int i = 0; i < 32; i++) printf("%02x", data->hs.s[i]);
-    puts("");
+    syslog(LOG_INFO, "[%-15s] ATTACH: %s", ip, key2hex(keystr, data->hs.s));
 
     HASH_FIND(hh, map, data->t, 32, a);
     if (a == NULL) {
-      warnx("target not found");
-      for (int i = 0; i < 32; i++) printf("%02x", data->t[i]);
-      puts("");
+      syslog(LOG_NOTICE, "[%-15s] target not found: %s", ip, key2hex(keystr, data->t));
       HEAD(p)->type = ERROR;
       DATA(p, ErrorData)->code = ERROR_NOT_FOUND;
     } else {
@@ -114,7 +114,7 @@ static void handle(braid_t b, int fd) {
       memcpy(DATA(p, ConnectData), &a->ads[a->n - 1].cd, sizeof(ConnectData));
 
       if (chsend(b, a->ads[a->n - 1].ch, (usize)&(ConnectData){ sa.sin_addr.s_addr, sa.sin_port })) {
-        warn("chsend failed");
+        syslog(LOG_ERR, "[%-15s] chsend failed while handling ATTACH: %m", ip);
         goto done;
       }
     }
@@ -126,13 +126,13 @@ static void handle(braid_t b, int fd) {
 
     clock_gettime(CLOCK_REALTIME, &ts);
     if (crypto_aead_unlock((uint8_t *)&data->ts_ms, data->mac2, ss, nonce, NULL, 0, (uint8_t *)&data->ts_ms, sizeof(data->ts_ms))) {
-      warnx("corrupted packet (timestamp)");
+      syslog(LOG_NOTICE, "[%-15s] corrupted packet (static)", ip);
       goto done;
     }
     nonce[23]++;
 
     if (((data->ts_ms > ts2ms(ts)) ? data->ts_ms - ts2ms(ts) : ts2ms(ts) - data->ts_ms) > 1000) {
-      warnx("advertise too old");
+      syslog(LOG_NOTICE, "[%-15s] advertise too old", ip);
       HEAD(p)->type = ERROR;
       DATA(p, ErrorData)->code = ERROR_INVALID_TIMESTAMP;
       goto send;
@@ -140,42 +140,42 @@ static void handle(braid_t b, int fd) {
 
     // TODO: check advertiser public key
 
-    printf("advertising from: ");
-    for (int i = 0; i < 32; i++) printf("%02x", data->hs.s[i]);
-    puts("");
+    syslog(LOG_INFO, "[%-15s] ADVERT: %s", ip, key2hex(keystr, data->hs.s));
     c = chcreate();
 
     HASH_FIND(hh, map, data->hs.s, 32, a);
     if (a == NULL) {
       if ((a = malloc(sizeof(struct advert))) == NULL) {
-        warn("malloc failed");
+        syslog(LOG_ERR, "[%-15s] malloc failed while handling ADVERT: %m", ip);
         goto done;
       }
 
       memset(a, 0, sizeof(struct advert));
       memcpy(a->pk, data->hs.s, 32);
 
-      a->n = 1;
       a->ads[0].cd.addr = sa.sin_addr.s_addr;
       a->ads[0].cd.port = sa.sin_port;
       a->ads[0].ch = c;
+      a->n = 1;
       HASH_ADD(hh, map, pk, 32, a);
     } else {
       if (a->n >= MAX_ADVERTS) {
-        warnx("too many adverts");
+        syslog(LOG_WARNING, "[%-15s] too many adverts", ip);
         HEAD(p)->type = ERROR;
         DATA(p, ErrorData)->code = ERROR_TOO_MANY_ADVERTS;
         goto send;
       } else {
-        a->n++;
         a->ads[a->n].cd.addr = sa.sin_addr.s_addr;
         a->ads[a->n].cd.port = sa.sin_port;
         a->ads[a->n].ch = c;
+        a->n++;
       }
     }
 
     HEAD(p)->type = CONNECT;
+    // TODO: tcp keepalive
     memcpy(DATA(p, ConnectData), (ConnectData *)chrecv(b, c), sizeof(ConnectData));
+    chdestroy(c);
 
     if (--a->n == 0) {
       HASH_DEL(map, a);
@@ -185,22 +185,26 @@ static void handle(braid_t b, int fd) {
 send:
   crypto_aead_lock(DATA(p, uint8_t), HEAD(p)->mac, ss, nonce, &HEAD(p)->type, 1, DATA(p, uint8_t), data_sz(p));
   crypto_wipe(ss, 32);
-  if (fdwrite(b, fd, &p, packet_sz(p)) != packet_sz(p)) warn("write to client");
+  if (fdwrite(b, fd, &p, packet_sz(p)) != packet_sz(p))
+    syslog(LOG_NOTICE, "[%-15s] request failed: send: %m", ip);
 done:
   close(fd);
   count--;
 }
 
-static void usage(const char *name) { errx(EX_USAGE, "usage: %s <port>", name); }
+static int usage(const char *name) {
+  fprintf(stderr, "usage: %s <port>", name);
+  return 1;
+}
 
 static void run_server(braid_t b, int s) {
   for (;;) {
     int c;
 
-    if ((c = tcpaccept(b, s)) < 0) { warn("accept failed"); }
+    if ((c = tcpaccept(b, s)) < 0) syslog(LOG_NOTICE, "accept failed: %m");
 
     if (count >= MAX_CONN) {
-      warnx("too many connections, dropping");
+      syslog(LOG_WARNING, "too many connections, dropping");
       close(c);
       continue;
     }
@@ -215,15 +219,18 @@ int server_main(int argc, char **argv) {
   int s;
   braid_t b;
 
-  if (argc != 2) usage(argv[0]);
+  if (argc != 2) return usage(argv[0]);
 
   snprintf(p, sizeof(p), "%s/.cherf2/rendez", getenv("HOME"));
   read_key(32, s_sk, p);
   crypto_x25519_public_key(s_pk, s_sk);
 
-  if ((s = tcplisten(NULL, atoi(argv[1]))) < 0) err(EX_OSERR, "tcplisten on port %s", argv[1]);
+  if ((s = tcplisten(NULL, atoi(argv[1]))) < 0) {
+    fprintf(stderr, "tcplisten on port %s: %s", argv[1], strerror(errno));
+    return 1;
+  }
 
-  printf("server listening on port %d, running as pid=%d\n", atoi(argv[1]), getpid());
+  signal(SIGPIPE, SIG_IGN);
 
   b = braidinit();
   braidadd(b, iovisor, 65536, "iovisor", CORD_SYSTEM, 0);
